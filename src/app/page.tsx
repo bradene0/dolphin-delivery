e600ff85a0dab2bb00cf9ddd46293b4459459e19
Braden E <bradene0@outlook.com>
2024-02-02 21:31:34 -0600

    mission statement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,16 +11,17 @@ export default function Home() {
     <a className="btn">Contact Us</a>
   </div>
 </div>
-<div className="hero min-h-screen" style={{backgroundImage: 'url()'}}>
+<div className="hero min-h-screen" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1491986542380-fdf7aebad979?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)'}}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
       <h1 className="mb-5 text-5xl font-bold">Welcome to Dolphin Delivery</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <p className="mb-5">We provide swift and efficient delivery of dolphins from zoos and aquariums TO the ocean. Not the other way around.</p>
       <button className="btn btn-primary">Get Started</button>
     </div>
   </div>
 </div>
+
 
     </main>
   );
